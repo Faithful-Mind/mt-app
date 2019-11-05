82088a012c08f35e48da8b66e2c4631e23e5feb8
Faithful-Mind <lpyy15@gmail.com>
2019-11-05 21:22:38 +0800

    完成首页滚动头条和面板

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="page-index">
     <el-row>
       <el-col :span="5"><Emenu /></el-col>
-      <el-col :span="19">2</el-col>
+      <el-col :span="19"><Life /></el-col>
     </el-row>
     <el-row>
       3
@@ -12,10 +12,12 @@
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
 
 export default {
   components: {
-    Emenu
+    Emenu,
+    Life
   }
 }
 </script>
