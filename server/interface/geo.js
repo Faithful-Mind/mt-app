2210f3a06ae734fb731bc5e221368dd00608b312
Faithful-Mind <lpyy15@gmail.com>
2019-11-10 23:03:36 +0800

    Geo接口实现（2）

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')
 
 const Menu = require('../dbs/models/menu')
 const Province = require('../dbs/models/province')
+const City = require('../dbs/models/city')
 const axios = require('./utils/axios')
 
 const router = new Router({ prefix: '/geo' })
@@ -37,6 +38,90 @@ router.get('/province', async (ctx) => {
   //   province: status === 200
   //     ? province
   //     : []
+  // }
+})
+
+router.get('/province/:id', async (ctx) => {
+  const city = await City.findOne({ id: ctx.params.id })
+
+  ctx.body = {
+    code: 0,
+    city: city.value.map((item) => {
+      return { province: item.province, id: item.id, name: item.name }
+    })
+  }
+  // const { status, data: { city } } = await axios.get(`http://cp-tools.cn/geo/province/${ctx.params.id}`)
+  // if (status === 200) {
+  //   ctx.body = {
+  //     city
+  //   }
+  // } else {
+  //   ctx.body = {
+  //     city: []
+  //   }
+  // }
+})
+
+router.get('/city', async (ctx) => {
+  let city = []
+  const result = await City.find()
+  result.forEach((item) => {
+    city = city.concat(item.value)
+  })
+  ctx.body = {
+    code: 0,
+    city: city.map((item) => {
+      return {
+        province: item.province,
+        id: item.id,
+        name: item.name === '市辖区' || item.name === '省直辖县级行政区划'
+          ? item.province
+          : item.name
+      }
+    })
+  }
+  // const { status, data: { city } } = await axios.get('http://cp-tools.cn/geo/city')
+  // if (status === 200) {
+  //   ctx.body = {
+  //     city
+  //   }
+  // } else {
+  //   ctx.body = {
+  //     city: []
+  //   }
+  // }
+})
+
+router.get('/hotCity', async (ctx) => {
+  const list = [
+    '北京市',
+    '上海市',
+    '广州市',
+    '深圳市',
+    '天津市',
+    '西安市',
+    '杭州市',
+    '南京市',
+    '武汉市',
+    '成都市'
+  ]
+  const result = await City.find()
+  let nList = []
+  result.forEach((item) => {
+    nList = nList.concat(item.value.filter(k => list.includes(k.name) || list.includes(k.province)))
+  })
+  ctx.body = {
+    hots: nList
+  }
+  // const { status, data: { hots } } = await axios.get('http://cp-tools.cn/geo/hotCity')
+  // if (status === 200) {
+  //   ctx.body = {
+  //     hots
+  //   }
+  // } else {
+  //   ctx.body = {
+  //     hots: []
+  //   }
   // }
 })
 
