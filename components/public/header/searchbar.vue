de861d72c2828abf7680d221fbfc5a14d2d3b9d1
Faithful-Mind <lpyy15@gmail.com>
2019-11-14 20:00:09 +0800

    产品列表页-页面联调

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -20,15 +20,25 @@
           <dl v-if="isHotPlace" class="hotPlace">
             <dt>热门搜索</dt>
             <dd v-for="(item, idx) in $store.state.home.hotPlace.slice(0, 5)" :key="idx">
-              {{ item.name }}
+              <a :href="'/products?keyword='+encodeURIComponent(item.name)">
+                {{ item.name }}
+              </a>
             </dd>
           </dl>
           <dl v-if="isSearchList" class="searchList">
-            <dd v-for="(item, idx) in searchList" :key="idx">{{ item.name }}</dd>
+            <dd v-for="(item, idx) in searchList" :key="idx">
+              <a :href="'/products?keyword='+encodeURIComponent(item.name)">
+                {{ item.name }}
+              </a>
+            </dd>
           </dl>
         </div>
         <p class="suggest">
-          <a v-for="(item, idx) in $store.state.home.hotPlace.slice(0, 5)" :key="idx" href="#">
+          <a
+            v-for="(item, idx) in $store.state.home.hotPlace.slice(0, 5)"
+            :key="idx"
+            :href="'/products?keyword='+encodeURIComponent(item.name)"
+          >
             {{ item.name }}
           </a>
         </p>
