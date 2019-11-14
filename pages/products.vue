1059a6583200f992f3f2527ee41c6d97d0290fa0
Faithful-Mind <lpyy15@gmail.com>
2019-11-14 17:43:50 +0800

    产品列表页-入口文件编写（1）

--- pages/products.vue
+++ pages/products.vue
@@ -1,15 +1,56 @@
 <template>
-  <div class="page-product">
-    1
-  </div>
+  <el-row class="page-product">
+    <el-col :span="19">
+      <crumbs :keyword="keyword" />
+      <category :types="types" :areas="areas" />
+      <list :list="list" />
+    </el-col>
+    <el-col :span="5">
+      <amap
+        v-if="point.length"
+        :width="230"
+        :height="290"
+        :point="point"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
+import Crumbs from '@/components/products/crumbs.vue'
+import Category from '@/components/products/category.vue'
+import List from '@/components/products/list.vue'
+import Amap from '@/components/public/map.vue'
 
+export default {
+  components: {
+    Crumbs,
+    Category,
+    List,
+    Amap
+  },
+  data () {
+    return {
+      list: [],
+      types: [],
+      areas: [],
+      keyword: '',
+      point: []
+    }
+  },
+  async asyncData (ctx) {
+    const keyword = ctx.query.keyword
+    const city = ctx.store.state.geo.position.city
+    const { status, data: { count, pois } } = await ctx.$axios.get('/search/resultsByKeywords', {
+      params: {
+        keyword,
+        city
+      }
+    })
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import "@/assets/css/products/index.scss";
 </style>
